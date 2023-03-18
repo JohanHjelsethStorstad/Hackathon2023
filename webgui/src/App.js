21d40bb2b13d2624c6c58899da48f574d5c6b53b
JohanHjelsethStorstad <82723971+JohanHjelsethStorstad@users.noreply.github.com>
2023-03-18 13:53:43 +0100

    add header to webgui

--- webgui/src/App.js
+++ webgui/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <Header />
+      </div>
+      <div className={styles.content}>
+        
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ webgui/src/components/Header.js
@@ -0,0 +1,9 @@
+import styles from "./Header.module.scss"
+
+function Header() {
+  return (
+    <div className={styles.wrapper}>Wine Monitor</div>
+  )
+}
+
+export default Header
